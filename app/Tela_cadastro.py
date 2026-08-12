74d0a368bef4050531987c3e0fb72b814c288129
Vinicius <viniciusspinola82@gmail.com>
2026-08-12 16:10:36 -0400

    adicionando tela de cadastro

---
+++ app/Tela_cadastro.py
@@ -0,0 +1,11 @@
+from PySide6.QtWidgets import QTabWidget
+from PySide6.QtWidgets import(
+    QWidget,
+    QVBoxLayout,
+    QFormLayout,
+    QLineEdit,
+    QComboBox,
+    QPushButton,
+    QLabel,
+    QMessageBox
+)
